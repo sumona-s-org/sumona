ccaa20a4aabd383c2092ed62cbee181a736ba017
Sumonamim <123830929+Sumonamim@users.noreply.github.com>
2023-03-17 20:07:25 +0600

    Create node.js

---
+++ node.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<title>W3.JS</title>
+<script src="https://www.w3schools.com/lib/w3.js"></script>
+<body>
+
+<p><button onclick="w3.toggleShow('#London')">Click to Toggle Hide/Show</button></p>
+
+<div id="London">
+  <h2>London</h2>
+  <p>London is the capital city of England.</p>
+</div>
+
+</body>
+</html>
